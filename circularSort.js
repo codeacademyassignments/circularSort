b4b5cca52866d32e1d3edc16322c88049c7655b4
Pulkit Agarwal <pulkit_agarwal-bng@external.mckinsey.com>
2019-01-25 05:14:59 +0530

    added method findIndexForNewElement: can return index for new element to insert

--- circularSort.js
+++ circularSort.js
@@ -5,4 +5,26 @@ const circularSort = (array) => {
   });
   return circularSortedArray;
 };
-module.exports = { circularSort };
+
+const findIndexForNewElement = (array, startIndex, endIndex, valueToSearch) => {
+  let i = startIndex;
+  if (startIndex === endIndex) {
+    if (startIndex === -1) {
+      return 0;
+    }
+  }
+  const lastIndexToSearch = ((endIndex + 1) % array.length);
+  while (i !== lastIndexToSearch) {
+    if (valueToSearch > array[i]) {
+      i += 1;
+    } else {
+      if (i === 0) {
+        return array.length - 1;
+      }
+      return i - 1;
+    }
+  }
+  return i;
+};
+console.log(findIndexForNewElement([-1, 1, 3, 5], 1, 3, 4));
+module.exports = { circularSort, findIndexForNewElement };
--- circularSort.test.js
+++ circularSort.test.js
@@ -8,3 +8,15 @@ describe('circularSort', () => {
     expect(functions.circularSort([1000])).toEqual([-1]);
   });
 });
+
+describe('findIndexForNewElement', () => {
+  it('should return index for new element to insert', () => {
+    expect(functions.findIndexForNewElement([-1, 1, 10, 20, 22, -1], 1, 4, 21)).toEqual(3);
+  });
+  it('should return index for new element to insert', () => {
+    expect(functions.findIndexForNewElement([-1, -1], -1, -1, 0)).toEqual(0);
+  });
+  it('should return index for new element to insert', () => {
+    expect(functions.findIndexForNewElement([-1, 2, 3], 1, 2, 1)).toEqual(0);
+  });
+});
