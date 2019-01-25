aeeb8a95ad5321484a7661f55b228fc6793ef329
Pulkit Agarwal <pulkit_agarwal-bng@external.mckinsey.com>
2019-01-25 10:46:56 +0530

    added circularSort: working code with few test cases

--- circularSort.js
+++ circularSort.js
@@ -30,6 +30,84 @@ const findIndexForNewElement = (array, startIndex, endIndex, valueToSearch) => {
     state: () => state,
   };
 };
+const findLeftOrRightShift = (array, indexForNewElement, startIndex, endIndex) => {
+  // console.log(indexForNewElement);
+  let smallerElements;
+  let largerElements;
+  if (indexForNewElement < startIndex) {
+    smallerElements = array.length - startIndex + indexForNewElement + 1;
+  } else {
+    smallerElements = indexForNewElement - startIndex + 1;
+  }
+  if (indexForNewElement > endIndex) {
+    largerElements = array.length - indexForNewElement + endIndex;
+  } else {
+    largerElements = endIndex - indexForNewElement;
+  }
+  let shiftState = 'rightShift';
+  if (smallerElements < largerElements) {
+    shiftState = 'leftShift';
+  }
+  return {
+    shift: () => shiftState,
+  };
+};
+
+const doShift = (array, shiftState, index, startIndex, endIndex) => {
+  // console.log(shiftState);
+  // console.log(index, startIndex, endIndex);
+  if (shiftState === 'leftShift') {
+    // console.log(index, startIndex, endIndex);
+    if (startIndex > 0) {
+      if (index < startIndex) {
+        for (let k = startIndex - 1; k < array.length; k += 1) {
+          array[k] = array[(k + 1) % array.length];
+        }
+        for (let k = 0; k < index; k += 1) {
+          array[k] = array[k + 1];
+        }
+      } else {
+        for (let k = startIndex - 1; k < index; k += 1) {
+          array[k] = array[k + 1];
+        }
+      }
+    } else {
+      array[array.length - 1] = array[0];
+      for (let k = 0; k < index; k += 1) {
+        array[k] = array[k + 1];
+      }
+    }
+  } else if (endIndex < (array.length - 1)) {
+    if (index > endIndex) {
+      // for (let k = startIndex - 1; k < array.length; k += 1) {
+      //   array[k] = array[(k + 1) % array.length];
+      // }
+      // for (let k = 0; k < index; k += 1) {
+      //   array[k] = array[k + 1];
+      // }
+      const lastValue = array[array.length - 1];
+      for (let k = (array.length - 1); k > index; k -= 1) {
+        array[k] = array[k - 1];
+      }
+      array[0] = lastValue;
+      for (let k = (endIndex + 1); k > 0; k -= 1) {
+        array[k] = array[k - 1];
+      }
+    } else {
+      for (let k = (endIndex + 1); k > index; k -= 1) {
+        array[k] = array[k - 1];
+      }
+    }
+  } else {
+    array[0] = array[array.length - 1];
+    for (let k = endIndex; k > index; k -= 1) {
+      array[k] = array[k - 1];
+    }
+  }
+  // console.log('done');
+  // console.log(array);
+  return array;
+};
 
 const circularSort = (inputArray) => {
   const circularSortedArray = [];
@@ -41,7 +119,9 @@ const circularSort = (inputArray) => {
   let endIndex = -1;
   let indexForNewElement;
   for (let i = 0; i < inputArray.length; i += 1) {
+    // console.log(endIndex);
     indexForNewElement = findIndexForNewElement(circularSortedArray, startIndex, endIndex, inputArray[i]);
+    // console.log('hjh', indexForNewElement.index());
     if (circularSortedArray[indexForNewElement.index()] === -1) {
       circularSortedArray[indexForNewElement.index()] = inputArray[i];
       if (startIndex === -1) {
@@ -53,18 +133,41 @@ const circularSort = (inputArray) => {
         endIndex = indexForNewElement.index();
       }
       result.push(circularSortedArray.slice());
-    }// else {
-    //   const shiftLeftOrRight = findLeftOrRightShift(circularSortedArray, indexForNewElement, startIndex, endIndex);
-    //   if (shiftLeftOrRight.shift() === 'leftShift') {
-    //     shiftArray(circularSortedArray, indexForNewElement, 'leftShift');
-    //   } else{
-    //     shiftArray(circularSortedArray, indexForNewElement, 'rightShift');
-    //   }
-    // }
+    } else {
+      const shiftLeftOrRight = findLeftOrRightShift(circularSortedArray, indexForNewElement.index(), startIndex, endIndex);
+      // console.log(shiftLeftOrRight.shift());
+      if (shiftLeftOrRight.shift() === 'leftShift') {
+        doShift(circularSortedArray, 'leftShift', indexForNewElement.index(), startIndex, endIndex);
+        // console.log('insidklk');
+        // console.log(circularSortedArray);
+        circularSortedArray[indexForNewElement.index()] = inputArray[i];
+        if (startIndex === 0) {
+          startIndex = inputArray.length - 1;
+        } else {
+          startIndex -= 1;
+        }
+        result.push(circularSortedArray.slice());
+      } else {
+        doShift(circularSortedArray, 'rightShift', (indexForNewElement.index() + 1) % inputArray.length, startIndex, endIndex);
+        // console.log('insid');
+        // console.log(circularSortedArray);
+        circularSortedArray[(indexForNewElement.index() + 1) % inputArray.length] = inputArray[i];
+        if (endIndex === (inputArray.length - 1)) {
+          endIndex = 0;
+        } else {
+          endIndex += 1;
+        }
+        result.push(circularSortedArray.slice());
+      }
+    }
   }
   return result;
 };
 
-
-console.log(findIndexForNewElement([-1, 1, 3, 5], 1, 3, 0).state());
-module.exports = { circularSort, findIndexForNewElement };
+// console.log(doShift([-1, -1, 2, 4, 5], 'left', 0, 2, 4));
+// console.log(doShift([1, -1, 2, 4, 5], 'right', 2, 1, 4));
+// console.log(findLeftOrRightShift([8, 9, -1, 2, 5], 4, 3, 1).shift());
+// console.log(findLeftOrRightShift([8, 9, -1, 2, 5], 3, 3, 1).shift());
+// console.log(findIndexForNewElement([-1, 1, 3, 5], 1, 3, 0).state());
+console.log(circularSort([25, 57, 37, 48, 12, 92, 86, 33]));
+module.exports = { circularSort, findIndexForNewElement, findLeftOrRightShift };
--- circularSort.test.js
+++ circularSort.test.js
@@ -7,11 +7,28 @@ describe('circularSort', () => {
   // it('should return array of -1 of size equals to input array', () => {
   //   expect(functions.circularSort([1000])).toEqual([-1]);
   // });
-  it('should return the correct sort sequence in circular fashion', () => {
-    expect(functions.circularSort([1, 2, 3])).toEqual([[1, -1, -1], [1, 2, -1], [1, 2, 3]]);
+  // it('should return the correct sort sequence in circular fashion', () => {
+  //   expect(functions.circularSort([1, 2, 3])).toEqual([[1, -1, -1], [1, 2, -1], [1, 2, 3]]);
+  // });
+  // it('should return the correct sort sequence in circular fashion', () => {
+  //   expect(functions.circularSort([4, 3, 5])).toEqual([[4, -1, -1], [4, -1, 3], [4, 5, 3]]);
+  // });
+
+  it('should return correct sequence', () => {
+    expect(functions.circularSort([25, 57, 37, 48, 12, 92, 86, 33])).toEqual([[25, -1, -1, -1, -1, -1, -1, -1],
+      [25, 57, -1, -1, -1, -1, -1, -1],
+      [25, 37, 57, -1, -1, -1, -1, -1],
+      [25, 37, 48, 57, -1, -1, -1, -1],
+      [25, 37, 48, 57, -1, -1, -1, 12],
+      [25, 37, 48, 57, 92, -1, -1, 12],
+      [25, 37, 48, 57, 86, 92, -1, 12],
+      [33, 37, 48, 57, 86, 92, 12, 25]]);
   });
-  it('should return the correct sort sequence in circular fashion', () => {
-    expect(functions.circularSort([4, 3, 5])).toEqual([[4, -1, -1], [4, -1, 3], [4, 5, 3]]);
+  it('should return correct sequence', () => {
+    expect(functions.circularSort([5, 10, 7, 8])).toEqual([[5, -1, -1, -1], [5, 10, -1, -1], [5, 7, 10, -1], [5, 7, 8, 10]]);
+  });
+  it('should return correct sequence', () => {
+    expect(functions.circularSort([5, 10, 7, 6])).toEqual([[5, -1, -1, -1], [5, 10, -1, -1], [5, 7, 10, -1], [6, 7, 10, 5]]);
   });
 });
 
