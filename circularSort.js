82b86f511110d85540784ab94f947ccbd5daa4d3
Pulkit Agarwal <pulkit_agarwal-bng@external.mckinsey.com>
2019-01-25 08:58:17 +0530

    modified circular wait: can calculate sorted sequence when all new elements are added on either ends

--- circularSort.js
+++ circularSort.js
@@ -1,30 +1,70 @@
-const circularSort = (array) => {
-  const circularSortedArray = [];
-  array.forEach(() => {
-    circularSortedArray.push(-1);
-  });
-  return circularSortedArray;
-};
-
 const findIndexForNewElement = (array, startIndex, endIndex, valueToSearch) => {
   let i = startIndex;
-  if (startIndex === endIndex) {
-    if (startIndex === -1) {
-      return 0;
-    }
+  let state = 'rightIndex';
+  if (startIndex === endIndex && startIndex === -1) {
+    return {
+      index: () => 0,
+      state: () => 'bothside',
+    };
   }
   const lastIndexToSearch = ((endIndex + 1) % array.length);
   while (i !== lastIndexToSearch) {
     if (valueToSearch > array[i]) {
-      i += 1;
+      i = (i + 1) % array.length;
     } else {
+      state = 'leftIndex';
       if (i === 0) {
-        return array.length - 1;
+        return {
+          index: () => array.length - 1,
+          state: () => state,
+        };
       }
-      return i - 1;
+      return {
+        index: () => i - 1,
+        state: () => state,
+      };
     }
   }
-  return i;
+  return {
+    index: () => i,
+    state: () => state,
+  };
 };
-console.log(findIndexForNewElement([-1, 1, 3, 5], 1, 3, 4));
+
+const circularSort = (inputArray) => {
+  const circularSortedArray = [];
+  inputArray.forEach(() => {
+    circularSortedArray.push(-1);
+  });
+  const result = [];
+  let startIndex = -1;
+  let endIndex = -1;
+  let indexForNewElement;
+  for (let i = 0; i < inputArray.length; i += 1) {
+    indexForNewElement = findIndexForNewElement(circularSortedArray, startIndex, endIndex, inputArray[i]);
+    if (circularSortedArray[indexForNewElement.index()] === -1) {
+      circularSortedArray[indexForNewElement.index()] = inputArray[i];
+      if (startIndex === -1) {
+        startIndex = indexForNewElement.index();
+        endIndex = indexForNewElement.index();
+      } else if (indexForNewElement.state() === 'leftIndex') {
+        startIndex = indexForNewElement.index();
+      } else {
+        endIndex = indexForNewElement.index();
+      }
+      result.push(circularSortedArray.slice());
+    }// else {
+    //   const shiftLeftOrRight = findLeftOrRightShift(circularSortedArray, indexForNewElement, startIndex, endIndex);
+    //   if (shiftLeftOrRight.shift() === 'leftShift') {
+    //     shiftArray(circularSortedArray, indexForNewElement, 'leftShift');
+    //   } else{
+    //     shiftArray(circularSortedArray, indexForNewElement, 'rightShift');
+    //   }
+    // }
+  }
+  return result;
+};
+
+
+console.log(findIndexForNewElement([-1, 1, 3, 5], 1, 3, 0).state());
 module.exports = { circularSort, findIndexForNewElement };
--- circularSort.test.js
+++ circularSort.test.js
@@ -1,22 +1,28 @@
 const functions = require('./circularSort.js');
 
 describe('circularSort', () => {
-  it('should return array of -1 of size equals to input array', () => {
-    expect(functions.circularSort([3, 2, 1])).toEqual([-1, -1, -1]);
+  // it('should return array of -1 of size equals to input array', () => {
+  //   expect(functions.circularSort([3, 2, 1])).toEqual([-1, -1, -1]);
+  // });
+  // it('should return array of -1 of size equals to input array', () => {
+  //   expect(functions.circularSort([1000])).toEqual([-1]);
+  // });
+  it('should return the correct sort sequence in circular fashion', () => {
+    expect(functions.circularSort([1, 2, 3])).toEqual([[1, -1, -1], [1, 2, -1], [1, 2, 3]]);
   });
-  it('should return array of -1 of size equals to input array', () => {
-    expect(functions.circularSort([1000])).toEqual([-1]);
+  it('should return the correct sort sequence in circular fashion', () => {
+    expect(functions.circularSort([4, 3, 5])).toEqual([[4, -1, -1], [4, -1, 3], [4, 5, 3]]);
   });
 });
 
 describe('findIndexForNewElement', () => {
   it('should return index for new element to insert', () => {
-    expect(functions.findIndexForNewElement([-1, 1, 10, 20, 22, -1], 1, 4, 21)).toEqual(3);
+    expect(functions.findIndexForNewElement([-1, 1, 10, 20, 22, -1], 1, 4, 21).index()).toEqual(3);
   });
   it('should return index for new element to insert', () => {
-    expect(functions.findIndexForNewElement([-1, -1], -1, -1, 0)).toEqual(0);
+    expect(functions.findIndexForNewElement([-1, -1], -1, -1, 0).index()).toEqual(0);
   });
   it('should return index for new element to insert', () => {
-    expect(functions.findIndexForNewElement([-1, 2, 3], 1, 2, 1)).toEqual(0);
+    expect(functions.findIndexForNewElement([-1, 2, 3], 1, 2, 1).index()).toEqual(0);
   });
 });
